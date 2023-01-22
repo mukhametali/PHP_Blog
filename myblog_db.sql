d05154f17105af090ad30d16e99513c3eef65852
mukhametali <m.kussaiynov1996@gmail.com>
2023-01-22 17:00:29 +0600

    sql

--- myblog_db.sql
+++ myblog_db.sql
@@ -3,13 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2022 at 09:42 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
